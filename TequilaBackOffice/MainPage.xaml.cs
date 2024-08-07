102c7b63d3c3d38713c3369653a9028d780f7fef
Hafiz <mdhafizul3131@gmail.com>
2024-08-08 00:59:20 +0600

    Add project files.

---
+++ TequilaBackOffice/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TequilaBackOffice
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
